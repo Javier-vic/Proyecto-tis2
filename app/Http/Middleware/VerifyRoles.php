63064543ba958b44ed2c0ff36c82bf97eb1066d3
Bernardo Fernández <andres_bernardo15@hotmail.com>
2022-05-19 14:56:04 -0400

    fix middleware

--- app/Http/Middleware/VerifyRoles.php
+++ app/Http/Middleware/VerifyRoles.php
@@ -8,7 +8,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\Models\permit;
-class VerifyRoles
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+
+class VerifyRoles extends Middleware
 {
     /**
      * Handle an incoming request.
@@ -19,21 +22,21 @@ class VerifyRoles
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-        if(!Auth::check()){
-            return redirect(RouteServiceProvider::HOME);
-        }   
         $id_role =$request->user()->id_role;
-        $ruta = $request->path();
+        $ruta = strtok($request->path(),'/');
+        
         $permits = DB::Table('role_permit')
             ->where('role_permit.id_role','=',$id_role)
             ->join('permits','permits.id','=','role_permit.id_permit')
             ->select('permits.tipe_permit')
             ->get();
+        
         foreach($permits as $permit){
             if($permit->tipe_permit == $ruta){
                 return $next($request);
             }  
         }
+        
         return redirect(RouteServiceProvider::HOME);
     }
 }
