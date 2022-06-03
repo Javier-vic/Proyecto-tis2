8c8f7097d924742e8966f77c842dd07b13e33759
Feelingoodbro <85776365+Feelingoodbro@users.noreply.github.com>
2022-06-03 17:37:52 -0400

    Avance landing page

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\category_product;
+use Illuminate\Support\Facades\DB;
+use App\Models\landing;
+use Illuminate\Http\Request;
+
+class LandingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //OBTIENE TODAS LAS CATEGORÍAS
+        $category_products = category_product::all();
+        ////////////////////////////////////////////////
+
+        //OBTIENE LOS PRODUCTOS Y ASOCIADOS A SU CATEGORÍA
+        $productAvailable = DB::table('products')
+            ->whereNull('products.deleted_at')
+            ->join('category_products', 'category_products.id', '=', 'products.id_category_product')
+            ->select(
+                'products.id',
+                'products.stock',
+                'products.name_product',
+                'products.description',
+                'products.image_product',
+                'category_products.name as category',
+                'category_products.id as category_id',
+                'products.price'
+
+            )
+            ->orderBy('products.id')
+            ->get();
+        ////////////////////////////////////////////////
+
+        //OBTIENE LAS CATEGORÍAS QUE ESTÁN DISPONIBLES SOLAMENTE ( con productos en stock)
+        $categoryAvailable = DB::table('category_products')
+            ->whereExists(function ($query) {
+                $query->select(DB::raw(1))
+                    ->from('products')
+                    ->whereColumn('products.id_category_product', 'category_products.id');
+            })
+            ->get();
+        ////////////////////////////////////////////////
+        return view('usuario.landing.landing', compact('category_products', 'categoryAvailable', 'productAvailable'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\landing  $landing
+     * @return \Illuminate\Http\Response
+     */
+    public function show(landing $landing)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\landing  $landing
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(landing $landing)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\landing  $landing
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, landing $landing)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\landing  $landing
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(landing $landing)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+<<<<<<< Updated upstream
+=======
+use App\Http\Controllers\CategorySupplyController;
+use App\Http\Controllers\SupplyController;
+>>>>>>> Stashed changes
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\CouponController;
 use App\Http\Controllers\ProductController;
@@ -7,6 +12,11 @@ use App\Http\Controllers\MapController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\CategoryProductController;
 use App\Http\Controllers\UserController;
+<<<<<<< Updated upstream
+=======
+use App\Http\Controllers\LandingController;
+use App\Http\Controllers\worker;
+>>>>>>> Stashed changes
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\VerifyRoles;
 /*
@@ -30,6 +40,7 @@ Auth::routes();
 Route::get('/coupon/refresh/coupon', [\App\Http\Controllers\CouponController::class, 'refreshCoupons'])->name('coupon.refresh.coupon');
 
 
+<<<<<<< Updated upstream
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('roles', RoleController::class)->middleware([]);
 Route::resource('product', ProductController::class);
@@ -49,3 +60,49 @@ Route::get('/categoryProduct/modal/edit', [\App\Http\Controllers\CategoryProduct
 Route::get('/permitsofrole', [\App\Http\Controllers\RoleController::class, 'getPermits'])->name('permits.roles');
 //DataTables
 Route::get('/dataTableRole', [\App\Http\Controllers\RoleController::class, 'dataTable'])->name('dataTable.Roles');
+=======
+Route::middleware(['auth', 'verifyrole'])->group(function () {
+    //GET
+    Route::get('/coupon/refresh/coupon', [\App\Http\Controllers\CouponController::class, 'refreshCoupons'])->name('coupon.refresh.coupon');
+    Route::get('/product/productView', [ProductController::class, 'productView'])->name('product.view');
+    Route::get('/product/productModalEdit', [ProductController::class, 'productModalEdit'])->name('product.modal.edit');
+    Route::get('/product/productView', [ProductController::class, 'productView'])->name('product.view');
+    Route::get('/product/productModalEdit', [ProductController::class, 'productModalEdit'])->name('product.modal.edit');
+    Route::get('/category_product/store/category', [CategoryProductController::class, 'store_category_product'])->name('categoryProduct');
+    Route::get('/category_product/modal/edit', [CategoryProductController::class, 'categoryProductModalEdit'])->name('category.product.modal.edit');
+    Route::get('/roles/permitsofrole', [RoleController::class, 'getPermits'])->name('permits.roles');
+    Route::get('/roles/dataTableRole', [RoleController::class, 'dataTable'])->name('dataTable.Roles');
+    Route::get('/asist/dataTable', [AsistController::class, 'dataTable'])->name('dataTable.asist');
+    Route::get('/product/productModalEdit', [\App\Http\Controllers\ProductController::class, 'productModalEdit'])->name('product.modal.edit');
+    Route::get('/product/productView', [\App\Http\Controllers\ProductController::class, 'productView'])->name('product.view');
+    Route::get('/worker/dataTable', [worker::class, 'dataTableWorkers'])->name('datatable.workers');
+    Route::get('/worker/getWorker', [worker::class, 'getWorker'])->name('worker.getWorker');
+    Route::get('/order/orderview', [\App\Http\Controllers\OrderController::class, 'getview'])->name('order.view');
+    //POST
+    Route::post('/asist/finish/{asist}', [AsistController::class, 'finishAsist'])->name('finish.asist');
+    Route::post('/order/addproduct', [\App\Http\Controllers\OrderController::class, 'addproduct'])->name('order.addproduct');
+    Route::post('/order/selectproduct', [\App\Http\Controllers\OrderController::class, 'selectproduct'])->name('order.selectproduct');
+    Route::post('/product/productModalEditStore/{product}', [ProductController::class, 'productModalEditStore'])->name('product.modal.edit.store');
+
+
+    //RESOURCE
+    Route::resource('category_supply', CategorySupplyController::class);
+    Route::resource('supply', SupplyController::class);
+    Route::resource('worker', worker::class);
+    Route::resource('asist', AsistController::class);
+    Route::resource('order', OrderController::class);
+    Route::resource('roles', RoleController::class);
+    Route::resource('product', ProductController::class);
+    Route::resource('category_product', CategoryProductController::class);
+    Route::resource('coupon', CouponController::class);
+    Route::resource('user', UserController::class);
+    Route::resource('map', MapController::class);
+});
+
+//RUTAS PARA LA VISTA DE USUARIOS
+Route::resource('landing', LandingController::class);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// RUTAS DE ORDENES
+// RUTA DE ROLES
+>>>>>>> Stashed changes
