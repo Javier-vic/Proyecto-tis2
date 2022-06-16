d6e102ce9bf649cfc07a1ff6c6e4039125338c1f
Bernardo Fernández <andres_bernardo15@hotmail.com>
2022-06-15 21:28:33 -0400

    fix redirect

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,6 +14,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        dd("rasd");
         if (!$request->expectsJson()) {
             return route('login');
         }
