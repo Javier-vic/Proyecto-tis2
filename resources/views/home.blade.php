d2d50126a9fba8d635d8f1ac6b01bd6e70dcab65
Javier-vic <jvictoriano125@gmail.com>
2022-06-23 12:46:35 -0400

    Iconos cantidad critica

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.navbar')
 
 @section('content')
+<div>
+    <i class="fa-solid fa-circle-exclamation text-danger"></i>
+
+    <i class="fa-solid fa-triangle-exclamation text-warning"></i>
+</div>
 @endsection
