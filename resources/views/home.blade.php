2418978d716f5e7620c9ee5afe3829386b94d433
Javier-vic <jvictoriano125@gmail.com>
2022-06-23 12:57:59 -0400

    Diseño iconos cantidad critica

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,30 @@
 
 @section('content')
 <div>
-    <i class="fa-solid fa-circle-exclamation text-danger"></i>
-
-    <i class="fa-solid fa-triangle-exclamation text-warning"></i>
+    <div class="border p-2">
+        
+        <div class="d-inline-block m-1">
+            <div class="text-center">
+                <i class="fa-solid fa-circle-exclamation text-danger fs-1"></i>
+                <div>
+                    <span class="text-danger">
+                        Insumo en cero
+                    </span>
+                </div>
+            </div>
+        </div>
+    
+        <div class="d-inline-block m-1">
+            <div class="text-center">
+                <i class="fa-solid fa-triangle-exclamation text-warning fs-1"></i>
+                <div>
+                    <span class="text-warning">
+                        insumo bajo en cantidad
+                    </span>
+                </div>
+            </div>
+        </div>
+        
+    </div>
 </div>
 @endsection
