bbaa84595a099180bd61d9f0f8443dfb0357c6bf
Javier-vic <93997406+Javier-vic@users.noreply.github.com>
2022-07-01 16:33:11 -0400

    notificaciones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,30 +2,5 @@
 
 @section('content')
 <div>
-    <div class="border p-2">
-        
-        <div class="d-inline-block m-1">
-            <div class="text-center">
-                <i class="fa-solid fa-circle-exclamation text-danger fs-1"></i>
-                <div>
-                    <span class="text-danger">
-                        Insumo en cero
-                    </span>
-                </div>
-            </div>
-        </div>
-    
-        <div class="d-inline-block m-1">
-            <div class="text-center">
-                <i class="fa-solid fa-triangle-exclamation text-warning fs-1"></i>
-                <div>
-                    <span class="text-warning">
-                        insumo bajo en cantidad
-                    </span>
-                </div>
-            </div>
-        </div>
-        
-    </div>
 </div>
 @endsection
