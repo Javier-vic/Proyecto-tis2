869bc06e9052e149142ab672e724b0169697e7e0
Javier-vic <93997406+Javier-vic@users.noreply.github.com>
2022-07-24 20:41:47 -0400

    Arreglo pedido
    Arreglos de visualizacion de estado de pedido

--- resources/views/Usuario/Landing/findOrder.blade.php
+++ resources/views/Usuario/Landing/findOrder.blade.php
@@ -12,20 +12,20 @@
             <div class="text-center my-4">
                 <form action="{{route('show.order')}}" method="get">
                     @csrf
-                    <input type="number" class="" id="id" name="id" value="{{$id}}" >
+                    <input type="number" class="" id="id" name="id" value="" >
                     <button type="" class="border border-danger p-1 px-2">                
                         <i class="fa-solid fa-magnifying-glass text-danger btn-find-order"></i>
                     </button>
                 </form>
             </div>
-            <div id="content-order">
+            <div id="content-order">               
                 @foreach($order as $data)
                 <h5 class="m-2 my-3 fw-bold">Nombre de la orden: <span class="fw-normal">{{$data->name_order}}</span></h5>
                 <h5 class="m-2 my-3 fw-bold">Total del pedido: <span class="fw-normal">{{$data->total}}</span></h5>
                 <h5 class="m-2 my-3 fw-bold">Estado del pedido: <span class="fw-normal">{{$data->order_status}}</span></h5>
                 <h5 class="m-2 my-3 fw-bold">Metodo de pago: <span class="fw-normal">{{$data->payment_method}}</span></h5>
                 <h5 class="m-2 my-3 fw-bold">Retiro: <span class="fw-normal">{{$data->pick_up}}</span></h5>
-                @endforeach
+                @endforeach               
             </div>
         </div>
         <div class="col-lg-3"></div>
